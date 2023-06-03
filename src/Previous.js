47bb3f8d9bbfc2908ee001d6a03c0e990b92ebee
Atikur Rahman <46051298+MohammadAtikurRahman@users.noreply.github.com>
2023-06-03 12:42:56 +0600

    download button is okay

--- src/Previous.js
+++ src/Previous.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Papa from 'papaparse';
+
 import axios from "axios";
 import {
   Table,
@@ -49,76 +51,101 @@ class Previous extends Component {
     });
   };
 
-  downloadCSV = () => {
-    axios
-      .get("http://localhost:2000/get-school")
-      .then((response) => {
-        const { data } = response;
-        let csvContent = "data:text/csv;charset=utf-8,";
+  // downloadCSV = () => {
+  //   axios
+  //     .get("http://localhost:2000/get-school")
+  //     .then((response) => {
+  //       const { data } = response;
+  //       let csvContent = "data:text/csv;charset=utf-8,";
 
-        // Add beneficiary data rows
-        const beneficiaryHeaders = [
-          "User Name",
-          "EIIN",
-          "School Name",
-          "PC ID",
-          "Lab ID",
-        ];
-        csvContent += beneficiaryHeaders.join(",") + "\r\n";
-        data.beneficiary.forEach((item) => {
-          const userName = `"${(item.m_nm || "").replace(/"/g, '""')}"`;
-          const eiin = item.beneficiaryId;
-          const name = item.name;
+  //       // Add beneficiary data rows
+  //       const beneficiaryHeaders = [
+  //         "User Name",
+  //         "EIIN",
+  //         "School Name",
+  //         "PC ID",
+  //         "Lab ID",
+  //       ];
+  //       csvContent += beneficiaryHeaders.join(",") + "\r\n";
+  //       data.beneficiary.forEach((item) => {
+  //         const userName = `"${(item.m_nm || "").replace(/"/g, '""')}"`;
+  //         const eiin = item.beneficiaryId;
+  //         const name = item.name;
 
-          const pcId = item.u_nm;
-          const labId = item.f_nm;
-          const row = [userName, eiin, name, pcId, labId];
-          csvContent += row.join(",") + "\r\n";
-        });
+  //         const pcId = item.u_nm;
+  //         const labId = item.f_nm;
+  //         const row = [userName, eiin, name, pcId, labId];
+  //         csvContent += row.join(",") + "\r\n";
+  //       });
 
-        // Add column headers for pc data
-        const pcHeaders = ["Start Time", "End Time", "Total Time"];
-        csvContent += pcHeaders.join(",") + "\r\n";
+  //       // Add column headers for pc data
+  //       const pcHeaders = ["Start Time", "End Time", "Total Time"];
+  //       csvContent += pcHeaders.join(",") + "\r\n";
 
-        // Add pc data rows
-        data.pc.forEach((item) => {
-          const startTime = `"${(item.earliestStart || "").replace(
-            /"/g,
-            '""'
-          )}"`;
-          const endTime = `"${(item.latestEnd || "").replace(/"/g, '""')}"`;
-          const totalTime = `"${(item.total_time || "").replace(/"/g, '""')}"`;
-          const row = [startTime, endTime, totalTime];
-          csvContent += row.join(",") + "\r\n";
-        });
+  //       // Add pc data rows
+  //       data.pc.forEach((item) => {
+  //         const startTime = `"${(item.earliestStart || "").replace(
+  //           /"/g,
+  //           '""'
+  //         )}"`;
+  //         const endTime = `"${(item.latestEnd || "").replace(/"/g, '""')}"`;
+  //         const totalTime = `"${(item.total_time || "").replace(/"/g, '""')}"`;
+  //         const row = [startTime, endTime, totalTime];
+  //         csvContent += row.join(",") + "\r\n";
+  //       });
 
-        // Extract school name for file renaming
-        const schoolName = data.beneficiary[0].name;
-        const eiin = data.beneficiary[0].beneficiaryId;
-        const pc_id = data.beneficiary[0].f_nm;
+  //       // Extract school name for file renaming
+  //       const schoolName = data.beneficiary[0].name;
+  //       const eiin = data.beneficiary[0].beneficiaryId;
+  //       const pc_id = data.beneficiary[0].f_nm;
 
-        // Extract month name from startTime
-        const monthName = new Date(data.pc[0].earliestStart).toLocaleString(
-          "default",
-          { month: "long" }
-        );
+  //       // Extract month name from startTime
+  //       const monthName = new Date(data.pc[0].earliestStart).toLocaleString(
+  //         "default",
+  //         { month: "long" }
+  //       );
 
-        // Include month name in the fileName
-        const fileName = `pc_${schoolName}_ein_${eiin}_Pc_Id_${pc_id}_Month_${monthName}.csv`;
+  //       // Include month name in the fileName
+  //       const fileName = `pc_${schoolName}_ein_${eiin}_Pc_Id_${pc_id}_Month_${monthName}.csv`;
 
-        // Create a download link
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", fileName);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch((error) => {
-        console.error("Error downloading CSV:", error);
+  //       // Create a download link
+  //       const encodedUri = encodeURI(csvContent);
+  //       const link = document.createElement("a");
+  //       link.setAttribute("href", encodedUri);
+  //       link.setAttribute("download", fileName);
+  //       document.body.appendChild(link);
+  //       link.click();
+  //       document.body.removeChild(link);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error downloading CSV:", error);
+  //     });
+  // };
+
+  downloadCSV = (month) => {
+    const { data } = this.state;
+  
+    // Filter the data for the specified month
+    const monthData = data.filter((item) => {
+      const itemMonth = new Date(item.earliestStart).toLocaleString("default", {
+        month: "long",
       });
+      return itemMonth.toLowerCase() === month.toLowerCase();
+    });
+  
+    // Convert the data to CSV
+    const csv = Papa.unparse(monthData);
+  
+    // Create a CSV Blob
+    const blob = new Blob([csv], { type: "text/csv" });
+  
+    // Create a link and click it to start the download
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `${month}_data.csv`;
+    link.click();
   };
+  
 
   downloadData = () => {
     const parser = new Parser();
@@ -135,36 +162,49 @@ class Previous extends Component {
 
     return (
       <div>
-        <div style={{ textAlign: "center" }}>
-          {data.length > 0 && (
-            <>
-              {Array.from(
-                new Set(
-                  data.map((item) =>
-                    new Date(item.earliestStart).toLocaleString("default", {
-                      month: "long",
-                    })
-                  )
-                )
-              ).map((month) => (
-                <div key={month} style={{ marginBottom: "10px" }}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => this.showDataByMonth(month)}
-                    style={{
-                      width: "200px",
-                      display: "block",
-                      marginBottom: "10px",
-                    }}
-                  >
-                    {month}'s PC Data
-                  </Button>
-                </div>
-              ))}
-            </>
-          )}
+      <div style={{ textAlign: "center" }}>
+  {data.length > 0 && (
+    <>
+      {Array.from(
+        new Set(
+          data.map((item) =>
+            new Date(item.earliestStart).toLocaleString("default", {
+              month: "long",
+            })
+          )
+        )
+      ).map((month) => (
+        <div key={month} style={{ marginBottom: "10px" }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.showDataByMonth(month)}
+            style={{
+              width: "200px",
+              display: "block",
+              marginBottom: "10px",
+            }}
+          >
+            {month}'s PC Data
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.downloadCSV(month)}
+            style={{
+              width: "200px",
+              display: "block",
+              marginBottom: "10px",
+            }}
+          >
+            Download {month}'s Data
+          </Button>
         </div>
+      ))}
+    </>
+  )}
+</div>
+
 
         {/* <Button
           variant="contained"
